e822f8acb25a202139e73bdfa41ebccf6eea3212
Juan Alfonsus Napitupulu <juanalfonsusn@gmail.com>
2023-07-29 10:05:54 +0700

    setup: layout for user setting screen only

--- PetDoorz-app/src/screens/customer/setting/UserSettingCustomer.jsx
+++ PetDoorz-app/src/screens/customer/setting/UserSettingCustomer.jsx
@@ -1,14 +1,50 @@
 import { StatusBar } from 'expo-status-bar'
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableHighlight, TouchableOpacity, View } from 'react-native'
 import { Button } from 'react-native-paper';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function UserSettingCustomer({ navigation }) {
   return (
-    <SafeAreaView style={ styles.container }>
-      <ScrollView>
-        <View>
-            <Text>User Setting Customer</Text>
-            <Button mode='contained' onPress={() => navigation.navigate('Customer Chat')}>Chats</Button>
+    <SafeAreaView>
+      <ScrollView contentContainerStyle={{ alignItems: 'center' }}>
+        {/* Card container, header */}
+        <View style={[card.container, card.shadowProp]}>
+          <Text style={card.title}>Ringola Botakz</Text>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 8 }}>
+            <Text style={card.balance}>Balance</Text>
+            <View style={{ flexDirection: 'row', gap: 10 }}>
+              <Text style={card.balance}>Rp. 300.000</Text>
+              <TouchableHighlight style={{ height: 23, width: 23, backgroundColor: 'gray', alignItems: 'center', justifyContent: 'center' }}>
+                <FontAwesome name="plus" size={20} color="white" />
+              </TouchableHighlight>
+            </View>
+          </View>
+        </View>
+
+        {/* Card container, body */}
+        <View style={[card.container, card.shadowProp]}>
+          <TouchableOpacity activeOpacity={0.8}>
+            <Text style={styles.menuTitle}>Settings</Text>
+            <View style={styles.horizontalMarker}></View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.8}>
+            <Text style={styles.menuTitle}>Bookings</Text>
+            <View style={styles.horizontalMarker}></View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.8}>
+            <Text style={styles.menuTitle}>Favorites</Text>
+            <View style={styles.horizontalMarker}></View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.8} onPress={() => navigation.navigate('Customer Chat')}>
+            <Text style={styles.menuTitle}>Chats</Text>
+            <View style={styles.horizontalMarker}></View>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.8}>
+            <Text style={styles.menuTitle}>Logout</Text>
+          </TouchableOpacity>
+
+          {/* tombol logout bingung */}
+          <Button mode='contained' style={{ marginTop: 12 }} onPress={() => console.log('logout')}>Logout</Button>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -20,13 +56,42 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center'
   },
-  screen: {
-    paddingHorizontal: 10,
-    marginTop: 20
+  menuTitle: {
+    fontSize: 18,
+    marginTop: 12,
+    marginBottom: 12
   },
-  button: {
-    height: 75,
-    justifyContent: 'center',
-    borderRadius: 18
+  menuRow: {
+
+  },
+  horizontalMarker: {
+    borderBottomColor: 'black',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    marginTop: 12,
+    elevation: 2,
+    opacity: 0.4
+  },
+})
+
+const card = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    marginVertical: 10,
+    borderRadius: 8,
+    width: '90%',
+    paddingBottom: 25
+  },
+  shadowProp: {
+    elevation: 3
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '600'
+  },
+  balance: {
+    fontSize: 16,
+    fontWeight: '500'
   }
 })
