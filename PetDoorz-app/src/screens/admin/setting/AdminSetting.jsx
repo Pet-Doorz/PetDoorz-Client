d941719b4b072856348da01289bcb6736d8c2a7d
Juan Alfonsus Napitupulu <juanalfonsusn@gmail.com>
2023-07-30 13:04:43 +0700

    setup: dialog on admin setting

--- PetDoorz-app/src/screens/admin/setting/AdminSetting.jsx
+++ PetDoorz-app/src/screens/admin/setting/AdminSetting.jsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar'
 import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableHighlight, TouchableOpacity, View } from 'react-native'
-import { Button } from 'react-native-paper';
-import { FontAwesome } from '@expo/vector-icons';
+import { Button, Dialog, Portal, PaperProvider } from 'react-native-paper'
+import { FontAwesome } from '@expo/vector-icons'
 import { useDispatch } from 'react-redux'
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { SET_ROLE } from '../../../store/actions/actionUser';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { SET_ROLE } from '../../../store/actions/actionUser'
+import { useEffect, useState } from 'react'
 
 
 export default function AdminSetting({ navigation }) {
@@ -15,43 +16,101 @@ export default function AdminSetting({ navigation }) {
         dispatch(SET_ROLE(''))
     }
 
+    const [visible, setVisible] = useState(false)
+    const [logout, setLogout] = useState(false)
+
+    const showDialog = (value) => {
+        if (value === 'logout') {
+            setLogout(true)
+        } else if (value === 'status') {
+            setVisible(true)
+        }
+    }
+    const hideDialog = (value) => {
+        if (value === 'logout') {
+            setLogout(false)
+        } else if (value === 'status') {
+            setVisible(false)
+        }
+    }
+
+    const handleStatusHotel = () => {
+        // Logic handleStatus Hotel
+        console.log('Active hotel!')
+        setVisible(false)
+    }
+
     return (
-        <SafeAreaView>
-            <ScrollView contentContainerStyle={{ alignItems: 'center' }}>
-                {/* Card container, header */}
-                <View style={[card.container, card.shadowProp]}>
-                    <Text style={card.title}>Juan Pet Hotel</Text>
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 8 }}>
-                        <Text style={card.balance}>Balance</Text>
-                        <View style={{ flexDirection: 'row', gap: 10 }}>
-                            <Text style={card.balance}>Rp. 2.500.000</Text>
-                            <TouchableHighlight style={{ height: 23, width: 23, backgroundColor: 'gray', alignItems: 'center', justifyContent: 'center' }}>
-                                <FontAwesome name="minus" size={20} color="white" />
-                            </TouchableHighlight>
+        <PaperProvider>
+            <SafeAreaView>
+                <ScrollView contentContainerStyle={{ alignItems: 'center' }}>
+                    {/* Dialog untuk aktifkan status hotel */}
+                    <Portal>
+                        <Dialog visible={visible} onDismiss={() => hideDialog('status')}>
+                            <Dialog.Title>Alert</Dialog.Title>
+                            <Dialog.Content>
+                                <Text variant="bodyMedium">Set hotel to Active?</Text>
+                            </Dialog.Content>
+                            <Dialog.Actions>
+                                <Button onPress={handleStatusHotel}>Ok</Button>
+                                <Button onPress={() => hideDialog('status')}>Cancel</Button>
+                            </Dialog.Actions>
+                        </Dialog>
+                    </Portal>
+
+                    {/* Dialog untuk logout */}
+                    <Portal>
+                        <Dialog visible={logout} onDismiss={() => hideDialog('logout')}>
+                            <Dialog.Title>Alert</Dialog.Title>
+                            <Dialog.Content>
+                                <Text variant="bodyMedium">Are you sure to Logout?</Text>
+                            </Dialog.Content>
+                            <Dialog.Actions>
+                                <Button onPress={handleLogout}>Ok</Button>
+                                <Button onPress={() => hideDialog('logout')}>Cancel</Button>
+                            </Dialog.Actions>
+                        </Dialog>
+                    </Portal>
+
+                    {/* Card container, header */}
+                    <View style={[card.container, card.shadowProp]}>
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10, alignItems: 'center' }}>
+                                <Text style={card.title}>Juan Pet Hotel</Text>
+                                {/* Tombol ini muncul harusnya dinamis dari data yg didapat */}
+                                <Button style={ styles.buttonActive } theme={{ colors: { primary: 'gray' } }} mode='contained' onPress={() => showDialog('status')}>Active</Button>
+                        </View>
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 8 }}>
+                            <Text style={card.balance}>Balance</Text>
+                            <View style={{ flexDirection: 'row', gap: 10 }}>
+                                <Text style={card.balance}>Rp. 2.500.000</Text>
+                                <TouchableHighlight style={{ height: 23, width: 23, backgroundColor: 'gray', alignItems: 'center', justifyContent: 'center' }}>
+                                    <FontAwesome name="minus" size={20} color="white" />
+                                </TouchableHighlight>
+                            </View>
                         </View>
                     </View>
-                </View>
 
-                {/* Card container, body */}
-                <View style={[card.container, card.shadowProp]}>
-                    <TouchableOpacity activeOpacity={0.8}>
-                        <Text style={styles.menuTitle}>Settings</Text>
-                        <View style={styles.horizontalMarker}></View>
-                    </TouchableOpacity>
-                    <TouchableOpacity activeOpacity={0.8}>
-                        <Text style={styles.menuTitle}>Bookings</Text>
-                        <View style={styles.horizontalMarker}></View>
-                    </TouchableOpacity>
-                    <TouchableOpacity activeOpacity={0.8}>
-                        <Text style={styles.menuTitle}>Chats</Text>
-                        <View style={styles.horizontalMarker}></View>
-                    </TouchableOpacity>
-                    <TouchableOpacity activeOpacity={0.8} onPress={handleLogout}>
-                        <Text style={styles.menuTitle}>Logout</Text>
-                    </TouchableOpacity>
-                </View>
-            </ScrollView>
-        </SafeAreaView>
+                    {/* Card container, body */}
+                    <View style={[card.container, card.shadowProp]}>
+                        <TouchableOpacity activeOpacity={0.8}>
+                            <Text style={styles.menuTitle}>Settings</Text>
+                            <View style={styles.horizontalMarker}></View>
+                        </TouchableOpacity>
+                        <TouchableOpacity activeOpacity={0.8}>
+                            <Text style={styles.menuTitle}>Bookings</Text>
+                            <View style={styles.horizontalMarker}></View>
+                        </TouchableOpacity>
+                        <TouchableOpacity activeOpacity={0.8}>
+                            <Text style={styles.menuTitle}>Chats</Text>
+                            <View style={styles.horizontalMarker}></View>
+                        </TouchableOpacity>
+                        <TouchableOpacity activeOpacity={0.8} onPress={() => showDialog('logout')}>
+                            <Text style={styles.menuTitle}>Logout</Text>
+                        </TouchableOpacity>
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
+        </PaperProvider>
     )
 }
 
@@ -75,6 +134,9 @@ const styles = StyleSheet.create({
         elevation: 2,
         opacity: 0.4
     },
+    buttonActive: {
+        borderRadius: 5,
+    }
 })
 
 const card = StyleSheet.create({
