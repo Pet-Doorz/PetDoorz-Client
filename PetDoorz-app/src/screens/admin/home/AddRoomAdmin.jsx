22b6b085c4af21b8c75de56f8a34018009e8353d
Juan Alfonsus Napitupulu <juanalfonsusn@gmail.com>
2023-07-31 16:46:59 +0700

    fix: fixing layout for add room admin

--- PetDoorz-app/src/screens/admin/home/AddRoomAdmin.jsx
+++ PetDoorz-app/src/screens/admin/home/AddRoomAdmin.jsx
@@ -1,20 +1,55 @@
-import { ScrollView, StyleSheet, View } from "react-native"
+import { ScrollView, StyleSheet, TouchableOpacity, View } from "react-native"
 import { Text } from "react-native"
 import { Button, TextInput } from "react-native-paper"
+import { FontAwesome } from '@expo/vector-icons'
 
 
 export default function AddRoomAdmin() {
     return (
         <ScrollView style={styles.container} contentContainerStyle={{ alignItems: 'center' }}>
             <View style={[{ marginTop: 10, gap: 9 }, styles.card]}>
-            <Text style={styles.title}>Add Room</Text>
+                <Text style={styles.title}>Add Room</Text>
                 <TextInput mode="outlined" label={'Room Name'}></TextInput>
                 <TextInput mode="outlined" label={'Room Description'}></TextInput>
                 <TextInput mode="outlined" label={'Room Capacity'} inputMode="numeric"></TextInput>
                 <TextInput mode="outlined" label={'Room Price'} inputMode="numeric"></TextInput>
-                <TextInput mode="outlined" label={'Image 1'}></TextInput>
-                <TextInput mode="outlined" label={'Image 2'}></TextInput>
-                <TextInput mode="outlined" label={'Image 3'}></TextInput>
+                {/* Input image */}
+                <View style={{ flexDirection: 'row' }}>
+                    <TextInput mode="outlined" label={'Image 1'} style={{ flex: 2 }} disabled value="http://awkdjawd"></TextInput>
+                    <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', gap: 20, marginStart: 20 }}>
+                        <TouchableOpacity activeOpacity={0.8} onPress={() => console.log('camera')}>
+                            <FontAwesome name="camera" size={30} color="black" />
+                        </TouchableOpacity>
+                        <TouchableOpacity activeOpacity={0.8} onPress={() => console.log('galery')}>
+                            <FontAwesome name="upload" size={30} color="black" />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                
+                <View style={{ flexDirection: 'row' }}>
+                    <TextInput mode="outlined" label={'Image 1'} style={{ flex: 2 }} disabled value="http://awkdjawd"></TextInput>
+                    <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', gap: 20, marginStart: 20 }}>
+                        <TouchableOpacity activeOpacity={0.8} onPress={() => console.log('camera')}>
+                            <FontAwesome name="camera" size={30} color="black" />
+                        </TouchableOpacity>
+                        <TouchableOpacity activeOpacity={0.8} onPress={() => console.log('galery')}>
+                            <FontAwesome name="upload" size={30} color="black" />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={{ flexDirection: 'row' }}>
+                    <TextInput mode="outlined" label={'Image 1'} style={{ flex: 2 }} disabled value="http://awkdjawd"></TextInput>
+                    <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', gap: 20, marginStart: 20 }}>
+                        <TouchableOpacity activeOpacity={0.8} onPress={() => console.log('camera')}>
+                            <FontAwesome name="camera" size={30} color="black" />
+                        </TouchableOpacity>
+                        <TouchableOpacity activeOpacity={0.8} onPress={() => console.log('galery')}>
+                            <FontAwesome name="upload" size={30} color="black" />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+
                 <Button mode="contained" style={{ borderRadius: 5, marginTop: 12 }} theme={{ colors: { primary: 'gray' } }}>Add</Button>
             </View>
         </ScrollView>
