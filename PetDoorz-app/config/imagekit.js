a37ae600962956f3a5ce9ff8e0de26ce9561a121
raymcnd <89778453+raymcnd@users.noreply.github.com>
2023-08-01 16:47:50 +0700

    fix: apply BASE_URL on imagekit config

--- PetDoorz-app/config/imagekit.js
+++ PetDoorz-app/config/imagekit.js
@@ -1,3 +1,5 @@
+import { BASE_URL } from "../src/store/actions/actionType"
+
 export const urlEndpoint = "https://ik.imagekit.io/raymcnd"
 export const publicKey = "public_YgtMaZkLj8pW1FbyrU31iiqeL4M="
-export const authenticationEndpoint = "https://7b70-2001-448a-4001-b897-ade7-5f21-563a-6a1a.ngrok-free.app/hotels/imagekit"
+export const authenticationEndpoint = BASE_URL + "/hotels/imagekit"
