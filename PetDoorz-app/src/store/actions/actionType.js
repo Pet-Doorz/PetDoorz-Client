ba74d8b101767289d8842b26dfbe45e76b363f90
DrunkenLee <michaelsinx.one@gmail.com>
2023-08-01 17:11:58 +0700

    feat: Admin Chat Side Chat Done

--- PetDoorz-app/src/store/actions/actionType.js
+++ PetDoorz-app/src/store/actions/actionType.js
@@ -9,4 +9,4 @@ export const SET_ALL_HOTEL = "customer/setAllHotelData";
 
 export const SET_ADMIN_DATA = "admin/setAdminDetailData";
 
-export const BASE_URL = "https://1c5b-111-94-95-220.ngrok-free.app"
+export const BASE_URL = "https://8adf-103-195-58-68.ngrok-free.app";
