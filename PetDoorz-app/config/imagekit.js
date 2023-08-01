6434119557713b0ada6cf442e48c4255a616c78c
raymcnd <89778453+raymcnd@users.noreply.github.com>
2023-08-01 16:38:15 +0700

    feat: add room

---
+++ PetDoorz-app/config/imagekit.js
@@ -0,0 +1,3 @@
+export const urlEndpoint = "https://ik.imagekit.io/raymcnd"
+export const publicKey = "public_YgtMaZkLj8pW1FbyrU31iiqeL4M="
+export const authenticationEndpoint = "https://7b70-2001-448a-4001-b897-ade7-5f21-563a-6a1a.ngrok-free.app/hotels/imagekit"
